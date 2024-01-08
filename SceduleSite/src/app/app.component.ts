4e0fd27b4cde16698d2385808fae674dfaf6a45f
EmmaAmos <elovestodraw@gmail.com>
2024-01-08 14:02:08 -0500

    added to root for server start. Added server.js file.

--- SceduleSite/src/app/app.component.ts
+++ SceduleSite/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,59 +6,68 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SceduleSite';
-
-  scheduleEvent!: any[];
-  formData: any={};
+  scheduleEvent: any[] = [];
+  formData: any = {};
   selectEvent: any;
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(){
-    this.getscheduleEvent();
+  ngOnInit() {
+    this.getscheduleEvent();  // Corrected the method call here
   }
 
   onSelect(scheduleEvent: any) {
     this.selectEvent = scheduleEvent;
   }
 
-  removeEvent(id: string){
-    const postData = {"scheduleEvent" : id}
-    this.http.post('http://localhost:3000/api/scheduleEvent/destroy', postData).subscribe(res =>{
-      console.log(res)
-    }, err =>{
-      console.log(err)
-    })
+  removeEvent(id: string) {
+    const postData = { "scheduleEvent": id };
+    this.http.post('http://localhost:3000/api/scheduleEvent/destroy', postData).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
-}
 
-addNewEvent(formData){
-  this.http.post('http://localhost:3000/api/scheduleEvent/store', formData).subscribe(res => {
-    console.log(res)
-  }, err =>{
-    console.log(err)
-  })
-}
-
-
-getscheduleEvent (){
-  this.http.get('http://localhost:3000/api/scheduleEvent').subscribe(data=>{
-    this.scheduleEvent = Object.values(data)
-    this.scheduleEvent = this.scheduleEvent[0]
-  })
-}
-
-updateEvent(id: string){
-  const putData = {
-    "eventID": id,
-    "name": this.scheduleEvent.name,
-    "event": this.scheduleEvent.event,
-    "date": this.scheduleEvent.date,
+  addNewEvent(formData) {
+    this.http.post('http://localhost:3000/api/scheduleEvent/store', formData).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
-  this.http.post('http://localhost:3000/api/scheduleEvent/update', putdata).subscribe(res =>{
-    this.selectEvent = null;
- }, err =>{
-  console.log(err)
-  });
+
+  getscheduleEvent() {
+    this.http.get('http://localhost:3000/api/scheduleEvent').subscribe(
+      data => {
+        this.scheduleEvent = Object.values(data);
+        this.scheduleEvent = this.scheduleEvent[0];
+      }
+    );
+  }
+
+  updateEvent(id: string) {
+    const putData = {
+      "eventID": id,
+      "name": this.scheduleEvent.name,
+      "event": this.scheduleEvent.event,
+      "date": this.scheduleEvent.date,
+    };
+    this.http.post('http://localhost:3000/api/scheduleEvent/update', putData).subscribe(
+      res => {
+        this.selectEvent = null;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
